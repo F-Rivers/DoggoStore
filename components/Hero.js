3980fe737e1aecb692296b91c682a825773b1d9f
F-Rivers <fxjxrx@gmail.com>
2022-09-20 10:21:08 -0700

    playing with fonts

--- components/Hero.js
+++ components/Hero.js
@@ -5,23 +5,23 @@ const Hero = () => {
 		<div className='my-48 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-72 text-center'>
 			<h1 className='font-extrabold text-gray-900'>
 				<p className='text-xl sm:text-6xl md:text-7xl'>DogGo</p>
-				<p className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-4xl sm:text-3xl md:text-4xl'>
+				<p className='text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-blue-500 to-teal-500 text-4xl sm:text-3xl md:text-4xl'>
 					The store for pets.
 				</p>
 			</h1>
-			<h2 className='mt-3 max-w-md mx-auto text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl'>
+			<h2 className='mt-3 max-w-md mx-auto text-blue-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl'>
 				Pet Friendly eCommerce
 			</h2>
 			<div className='mt-5 max-w-md mx-auto flex justify-center items-center md:mt-8'>
 				<Link href='#'>
 					{/* add link for sign up page */}
-					<a className='inline-flex items-center justify-center h-12 px-6 mr-6 font-medium py-3 border-transparent rounded-md text-white bg-gray-900 hover:bg-gray-800'>
+					<a className='inline-flex items-center justify-center h-12 px-6 mr-6 font-medium py-3 border-transparent rounded-md text-white bg-gray-900 hover:bg-blue-800'>
 						Sign Up
 					</a>
 				</Link>
 				<Link href='#'>
 					{/* add link for sign in */}
-					<a className='inline-flex items-center font-semibold text-gray-900 hover:text-gray-800'>
+					<a className='inline-flex items-center font-semibold text-gray-900 hover:text-blue-800'>
 						Sign In
 					</a>
 				</Link>
